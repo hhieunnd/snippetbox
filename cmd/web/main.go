5327b263e773d373a173a73fa501745d33734721
hieubk <hieundpython@gmail.com>
2024-08-30 18:34:24 +0700

    add config and log error

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,22 +1,44 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type config struct {
+	addr      string
+	staticDir string
+}
+
+type application struct {
+	infoLog  *log.Logger
+	errorLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
+	var cfg config
+	flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "Path to static assets")
+	flag.Parse()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	app := &application{
+		infoLog:  infoLog,
+		errorLog: errorLog,
+	}
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	srv := http.Server{
+		Addr:     cfg.addr,
+		ErrorLog: errorLog,
+		Handler:  app.routes(),
+	}
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", cfg.addr)
+
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
